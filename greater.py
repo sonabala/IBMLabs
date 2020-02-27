fa3aff52ed64ca15955140d6bcbf6750e27ae831
sonabala <61139769+sonabala@users.noreply.github.com>
2020-02-26 22:24:50 -1200

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,15 @@
+def compute_lcm (x,y):
+    if x>y:
+        greater = x
+    else:
+        greater = y
+    while True:
+        if(greater % x==0) and (greater % y==0):
+            lcm=greater
+            break
+            greater += 1
+    return lcm
+
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number"))
+print("the L.C.M.is",compute_lcm(num1,num2))
